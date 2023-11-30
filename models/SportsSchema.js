47c2fb00d9f619cd310a2deb70627ac6f082a583
Rahan Mohammed <rahanahmed1694@gmail.com>
2023-11-30 08:34:56 -0600

    Screenshot 7

--- models/SportsSchema.js
+++ models/SportsSchema.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
 const SportsSchema = mongoose.Schema({
 SportsCar: String,
-Sportsperson: String,
-sportsbike: String
+Sportsbike: {type: String,
+    minlength: 2,
+    maxlength: 10},
+Sportsperson:{type: String,
+minlength: 3,
+maxlength: 100}
 })
 module.exports = mongoose.model("Sports",
 SportsSchema)
--- routes/Sports1.js
+++ routes/Sports1.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page.
 router.get('/', function(req, res, next) { 
   res.render('Sports1', { title: 'Search results Sports1' });
-});
-module.exports = router;
+});  */
+
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  res.redirect("/login");
+  }
 
     /* GET detail Sports page */
     const Sports_controlers= require('../controllers/Sports1');
     router.get('/detail', Sports_controlers.Sports_view_one_Page);
 
     /* GET create Sports page */
-router.get('/create', Sports_controlers.Sports_create_Page);
+router.get('/create', secured, Sports_controlers.Sports_create_Page);
 
 /* GET create update page */
-router.get('/update', Sports_controlers.Sports_update_Page);
-router.get('/update', secured,Sports_controlers.costume_update_Page);
+router.get('/update', secured,Sports_controlers.Sports_update_Page);
+
 
 /* GET delete Sports page */
-router.get('/delete', Sports_controlers.Sports_delete_Page);
+router.get('/delete', secured, Sports_controlers.Sports_delete_Page);
+module.exports = router;
